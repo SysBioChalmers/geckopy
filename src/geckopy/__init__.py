5eeaa378d744a46d780750adca620f7f4bd9f078
Eduard Kerkhoven <eduardk@chalmers.se>
2026-04-23 21:38:46 +0200

    feat: restart porting process

--- src/geckopy/__init__.py
+++ src/geckopy/__init__.py
@@ -1,17 +1,2 @@
-"""geckopy – enzyme-constrained metabolic modeling in Python.
+"""geckopy: Enzyme-constrained genome-scale metabolic modeling in Python."""
 
-Provides :class:`~geckopy.core.ECModel`, :class:`~geckopy.core.ECData`, and
-:class:`~geckopy.core.Enzyme` as the core data structures for building and
-analysing enzyme-constrained genome-scale models (ecGEMs), following the
-conventions of the GECKO Toolbox (Sánchez et al. 2017; Domenzain et al. 2022).
-"""
-
-from geckopy.core.ec_model import ECData, ECModel
-from geckopy.core.enzyme import Enzyme
-
-
-__all__ = [
-    "ECData",
-    "ECModel",
-    "Enzyme",
-]
