d5d0d1348bb5ecb6e1e33bd654b5d3a434d32109
Eduard Kerkhoven <eduardk@chalmers.se>
2026-04-24 23:53:35 +0200

    feat: stage 6-8 of makeEcModel

--- src/geckopy/ec_model/pipeline/__init__.py
+++ src/geckopy/ec_model/pipeline/__init__.py
@@ -1,5 +1,10 @@
 """Pipeline stages of make_ec_model, ported from GECKO MATLAB."""
 from .expand import expand_model
+from .populate_ec import (
+    allocate_ec_for_catalyzed_reactions,
+    build_rxn_enzyme_coupling,
+    populate_enzyme_data,
+)
 from .preprocess import (
     convert_to_irreversible,
     invert_backwards_only_reactions,
@@ -7,8 +12,11 @@ from .preprocess import (
 )
 
 __all__ = [
+    "allocate_ec_for_catalyzed_reactions",
+    "build_rxn_enzyme_coupling",
     "convert_to_irreversible",
     "expand_model",
     "invert_backwards_only_reactions",
+    "populate_enzyme_data",
     "remove_pseudoreaction_gprs",
 ]
