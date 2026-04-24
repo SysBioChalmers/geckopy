b4cc0be5857a0b5f1a3144d5cab1d0549b62d5fc
Eduard Kerkhoven <eduardk@chalmers.se>
2026-04-25 00:21:12 +0200

    feat: implement make_ec_model

--- src/geckopy/__init__.py
+++ src/geckopy/__init__.py
@@ -1,5 +1,11 @@
 """geckopy: Enzyme-constrained genome-scale metabolic modeling in Python."""
 from geckopy.adapter import ModelAdapter, ModelParameters
-from geckopy.ec_model import EcData, EcModel
+from geckopy.ec_model import EcData, EcModel, make_ec_model
 
-__all__ = ["EcData", "EcModel", "ModelAdapter", "ModelParameters"]
+__all__ = [
+    "EcData",
+    "EcModel",
+    "ModelAdapter",
+    "ModelParameters",
+    "make_ec_model",
+]
