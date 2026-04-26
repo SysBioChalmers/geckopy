71baa206da90d9e52f71828fd37181dadf2af536
Eduard Kerkhoven <eduardk@chalmers.se>
2026-04-26 21:43:04 +0200

    feat: find_met_smiles

--- src/geckopy/databases/__init__.py
+++ src/geckopy/databases/__init__.py
@@ -4,11 +4,13 @@ from .complex_portal_loader import (
     ComplexPortalEntry,
     load_complex_portal_json,
 )
+from .pubchem import find_met_smiles
 from .uniprot_loader import UniprotDB, load_uniprot_tsv
 
 __all__ = [
     "ComplexPortalEntry",
     "UniprotDB",
+    "find_met_smiles",
     "get_complex_data",
     "load_complex_portal_json",
     "load_uniprot_tsv",
